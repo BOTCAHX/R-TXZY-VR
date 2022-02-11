d58ff663cafa7f6571253c6474bb913e9da61978
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2022-02-11 11:56:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -49,7 +49,7 @@ global.eror = '「❗」Server ERROR!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 global.watermark = '© R-Txzy' //change the watermark 
 global.image = 'https://telegra.ph/file/0bdd84bd5a8099a789828.jpg' //change the image
-global.thumbfoto = 'https://telegra.ph/file/ad40340695907adb17d40.png'
+global.thumbfoto = 'https://telegra.ph/file/74ee3213700db6534e59a.png'
 
 // image
 global.bank = 'https://telegra.ph/file/d5ddf4cc627bb0e6bc420.jpg'
