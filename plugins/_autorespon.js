f4229c718bf0e1368c4b2abcca674124029e4721
SF-BOTZ <94295494+Ipulpachri@users.noreply.github.com>
2022-03-23 08:28:25 +0700

    _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -72,7 +72,7 @@ handler.all = async function (m, { isBlocked }) {
     if (new Date() * 1 - setting.status > 1000) {
         let _uptime = process.uptime() * 1000
         let uptime = clockString(_uptime)
-        await this.setStatus(`💠 Mode bot aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} | Fachri Botz by Fachri×፝֟͜×`).catch(_ => _)
+        await this.setStatus(`💠 Mode bot aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'}`).catch(_ => _)
         setting.status = new Date() * 1
     }
 
