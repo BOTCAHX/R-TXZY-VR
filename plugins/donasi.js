1c58b5f39b342cb17f8e0de24d7525554c068ccd
SF-BOTZ <94295494+Ipulpachri@users.noreply.github.com>
2022-03-25 10:22:13 +0700

    donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -3,13 +3,14 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), `
 ┌─✧ 〔 Donasi • Emoney 〕
-├◌ GoPay: 085713041886 [ Fachri ]
-├◌ Dana: 085713041886 [ Fachri ]
-├◌ Pulsa: 081395861695 [ BOTCAHX ]
+├◌ GoPay: *085713041886* [ Fachri ]
+├◌ Dana: *085713041886* [ Fachri ]
+├◌ Pulsa: *081395861695* [ BOTCAHX ]
 └────···✧
 
+*Donasi Bang Biar Bot nya On 24 Jam*
 Terimakasih Banyak Untuk Yg Sudah Berdonasi !!
-`.trim(), DonasiBgBiarBotnyaOn24Jam, 'Menu', '.menu', m)
+`.trim(), watermark, 'Menu', '.menu', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
