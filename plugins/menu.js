8134b126904cdc33ba21ff69cc2d420bb5e8ed30
SF-BOTZ <94295494+Ipulpachri@users.noreply.github.com>
2022-04-05 08:45:36 +0700

    menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -433,7 +433,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
             return menu.help.map(help => {
               return body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
                 .replace(/%islimit/g, menu.limit ? '(Limit)' : '')
-                .replace(/%isPremium/g, menu.premium ? '(Premium)' : '')
+                .replace(/%isPremium/g, menu.limit ? '(Limit)' : '')
                 .trim()
             }).join('\n')
           }),
